e9c52fab980409f4a131b82ba422b2a3be4e70c9
DPK252000 <113545795+DPK252000@users.noreply.github.com>
2022-09-16 22:55:40 +0530

    Add files via upload

---
+++ Type_Conversion.Casting.py
@@ -0,0 +1,22 @@
+print(type(9))
+print(type(9.9))
+print(type('hello'))
+
+a=3
+print(a)
+print(str(2))
+p=9.9
+print(int(p))
+print(str(p))
+print(str(p)+'pavan')
+
+a='hi'
+print(type(a))
+k='9'
+print(type(k))
+
+y=9.9
+print(type(y))
+print(float(y))
+y=float(y)  #9.9
+print(int(y))
